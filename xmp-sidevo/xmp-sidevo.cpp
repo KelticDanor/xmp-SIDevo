96d6533342d5114a1d35a58e8c6bf4a023bb2096
Nathan Hindley <87258122+KelticDanor@users.noreply.github.com>
2026-08-29 16:39:40 +0700

    Add files via upload

--- xmp-sidevo/xmp-sidevo.cpp
+++ xmp-sidevo/xmp-sidevo.cpp
@@ -741,7 +741,7 @@ static void WINAPI SIDevo_GetGeneralInfo(char* buf)
 	}
 
 	buf += sprintf(buf, "%s\t%s\r", "Length", simpleLength(sidEngine.p_songlength, temp));
-	buf += sprintf(buf, "%s\t%s\r", "Library", "libsidplayfp-2.15.0");
+	buf += sprintf(buf, "%s\t%s\r", "Library", "libsidplayfp-2.16.1");
 }
 static inline unsigned char petscii2ascii(unsigned char ch)
 {
@@ -1341,7 +1341,7 @@ static void WINAPI SIDevo_About(HWND win)
 // plugin interface
 static XMPIN xmpin = {
 	0,
-	"SIDevo (v4.9.1)",
+	"SIDevo (v4.9.2)",
 	"SIDevo\0sid/mus/str",
 	SIDevo_About,
 	SIDevo_Config,
